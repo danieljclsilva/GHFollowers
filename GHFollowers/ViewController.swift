c76471faab32d13a7ccafca0df2a94ebd9dd472c
danieljclsilva <danieljclsilva@outlook.com>
2024-05-25 22:18:34 +0100

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Daniel Silva on 25/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
