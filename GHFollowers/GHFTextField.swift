b49bcf138d68086c994aefe97a4c664da6dd831a
danieljclsilva <danieljclsilva@outlook.com>
2024-05-26 21:58:47 +0100

    Add custom UITextField

---
+++ GHFollowers/GHFTextField.swift
@@ -0,0 +1,44 @@
+//
+//  GHFTextField.swift
+//  GHFollowers
+//
+//  Created by Daniel Silva on 26/05/2024.
+//
+
+import UIKit
+
+class GHFTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        self.placeholder = placeholder
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+    }
+}
