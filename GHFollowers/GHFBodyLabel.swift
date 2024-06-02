72c3449794e1f708dc2e8eec48de88e11923a32e
danieljclsilva <danieljclsilva@outlook.com>
2024-06-02 23:20:23 +0100

    Add custom body label

---
+++ GHFollowers/GHFBodyLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GHFBodyLabel.swift
+//  GHFollowers
+//
+//  Created by Daniel Silva on 02/06/2024.
+//
+
+import UIKit
+
+class GHFBodyLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+    }
+}
---
+++ GHFollowers/GHFTitleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GHFTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Daniel Silva on 02/06/2024.
+//
+
+import UIKit
+
+class GHFTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
