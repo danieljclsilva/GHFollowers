e633b508976db32370b6bd857ee7b9df58f1b28c
danieljclsilva <danieljclsilva@outlook.com>
2024-07-08 00:10:40 +0100

    Create follower and user models

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,8 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Daniel Silva on 07/07/2024.
+//
+
+import Foundation
---
+++ GHFollowers/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Daniel Silva on 07/07/2024.
+//
+
+import Foundation
