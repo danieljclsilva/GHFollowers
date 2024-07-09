ddd93fc5b2a0c13233a47fcdc1860e6b42620f27
danieljclsilva <danieljclsilva@outlook.com>
2024-07-09 22:24:19 +0100

    Fix endpoint bug

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -15,7 +15,7 @@ class NetworkManager {
     private init() {}
     
     func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
-        let endpoint = baseURL + "/users/\(username)/followers?per_page=\(perPage)&page=\(page)"
+        let endpoint = baseURL + "users/\(username)/followers?per_page=\(perPage)&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
             completed(nil, "This username created an invalid request. Please try again.")
