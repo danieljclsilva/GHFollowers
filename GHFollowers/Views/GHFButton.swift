cb8b67d600fd4373bc0adeb214b1e935ece142ac
danieljclsilva <danieljclsilva@outlook.com>
2024-07-09 22:23:10 +0100

    Fix custom button

--- GHFollowers/Views/GHFButton.swift
+++ GHFollowers/Views/GHFButton.swift
@@ -29,7 +29,7 @@ class GHFButton: UIButton {
         translatesAutoresizingMaskIntoConstraints = false
         
         layer.cornerRadius = 10
-        titleLabel?.textColor = .white
+        setTitleColor(.white, for: .normal)
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
     }
 }
